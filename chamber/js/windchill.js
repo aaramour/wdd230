6c8acc19fd1c54b8ea09ea8d254892ca8d1c243c
aaramour <aaramour@byui.edu>
2022-06-29 18:15:38 -0400

    fixed windchill

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -4,7 +4,8 @@ const windSpeed = document.querySelector('#windSpeed');
 const weatherDesc = document.querySelector('#weatherDesc');
 
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&appid=3349075b3198795c47ed031fb8761419`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=
+Inglis&units=Imperial&appid=3349075b3198795c47ed031fb8761419`;
 
 apiFetch(url);
 
@@ -13,7 +14,7 @@ async function apiFetch(apiURL) {
     const response = await fetch(apiURL);
     if (response.ok) {
       const data = await response.json();
-      // console.log(data);
+      console.log(data);
       displayResults(data);
     } else {
       throw Error(await response.text());
@@ -24,7 +25,8 @@ async function apiFetch(apiURL) {
 }
 
 function displayResults(weatherData) {
-  currentTemp.innerHTML = `${weatherData.main.temp.toFixed(
+  const temp = weatherData.main.temp;
+  currentTemp.innerHTML = `${temp.toFixed(
     0
   )}°F`;
   const imagesrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
@@ -34,23 +36,28 @@ function displayResults(weatherData) {
   weatherIcon.setAttribute("alt", desc);
   windSpeed.innerHTML = (`${windSpeedValue.toFixed(1)} mph`);
   weatherDesc.innerHTML = capitalize(desc);
+  let windChill = windChiller(temp, windSpeedValue);
+  document.querySelector("#windChill").textContent = windChill;
 }
 
 
 // let temp = parseFloat(document.querySelector("#temp").textContent);
-let windSpeedValue = parseFloat(document.querySelector("#windSpeed").textContent);
-let windChill = "";
-let windChillText = "";
+// let windSpeedValue = parseFloat(document.querySelector("#windSpeed").textContent);
+// let windChill = "";
+// let windChillText = "";
 
-if (currentTemp <= 50 && windSpeedValue > 3) {
-    windChill = 35.74 + (.6215 * currentTemp) - (35.75 * Math.pow(windSpeedValue, 0.16)) + (.4275 * currentTemp) * Math.pow(windSpeedValue, 0.16);
-    windChill = `${Math.round(windChill)}°F`;
-} else { windChill = "N/A"}
+function windChiller(temp, speed) {
+  if (temp <= 50 && speed > 3) {
+      windChill = 35.74 + (.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (.4275 * temp) * Math.pow(speed, 0.16);
+      windChill = `${windChill.toFixed(1)}°F`;
+      return windChill;
+  } else { return windChill = "N/A"}
+}
 
 function capitalize(string) {
     return string.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
   }
 
-document.querySelector("#windChill").textContent = windChill;
+
 
 
