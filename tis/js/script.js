7695e41f7c899d6663d244e100fbcff29a6b3cbf
aaramour <aaramour@byui.edu>
2022-07-13 17:08:28 -0400

    fhsggfg

---
+++ tis/js/script.js
@@ -0,0 +1,32 @@
+const hamburger_btn = document.querySelector("#hamburger");
+
+const nav = document.querySelector("nav");
+
+hamburger_btn.addEventListener(
+  "click",
+  (nav) => {
+    toggleMenu(document.querySelector("nav"), document.querySelector("#hamburger"));
+  },
+  false
+);
+
+function toggleMenu(item, button) {
+  console.log("The Menu Toggle Worked!");
+  console.log(item.classList);
+  if (item.classList.contains("closed")) {
+    item.removeAttribute("class");
+    item.setAttribute("class", "open");
+    button.removeAttribute("src");
+    button.setAttribute(
+      "src",
+      "https://aaramour.github.io/wdd230/tis/images/x_btn.png"
+    );
+  } else {
+    item.setAttribute("class", "closed");
+    button.removeAttribute("src");
+    button.setAttribute(
+      "src",
+      "https://aaramour.github.io/wdd230/tis/images/burger_btn.png"
+    );
+  }
+}
